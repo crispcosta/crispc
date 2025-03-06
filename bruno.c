73bbee970c12b0db1d15077cddc28c00b8e09124
crispcosta <cristianacostta58@gmail.com>
2025-03-06 10:58:57 -0300

    Update bruno.c

--- bruno.c
+++ bruno.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    double saldoBancario = 12345.67;
+    char inicial = 'A';
+    char nome[20] = "Bruno";
+ 
+    printf("Idade: %d anos\n", idade
+    printf("Altura: %.2f metros\n", 
+    printf("Saldo Bancário: %.2f rea
+    printf("Inicial do Nome: %c\n", 
+    printf("Nome: %s\n", nome);
+
+    return 0;
+}
+//Idade:25 anos
+//Altura:1.75 metros
+//saldo bancario:12345.67 reais
+//Inicial do nome:A
+//Nome:Bruno
 
